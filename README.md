58fcbac517a5ebe140676af03ec4a5bd1aab0dba
Nathaniel Korous <natework@Nathaniels-MacBook-Pro-2.local>
2018-08-02 12:39:41 -0600

    added bootstrap and hello world

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Gist
+
+my notes in doing this app are found at
+[https://gist.github.com/nKorous/b7551d1f90cbfe00dd1b61702659982b/]
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Below you will find some information on how to perform common tasks.<br>
--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <div style={{textAlign: 'center'}}>
+        <h1>Hello World</h1>
+      </div>
     );
   }
 }
